8d4ee3c93ba271f8ed27b9c69c5fe9d3b8166e64
Miguezpz <miguezpzmx@gmail.com>
2024-09-18 23:49:54 -0600

    cpimg error cannot copy to folder fixed

--- cpimg.sh
+++ cpimg.sh
@@ -13,7 +13,7 @@ echo "Directorio my-images creado" &&
 
 #Iniciamos busqueda de imagenes y copiamos a la ruta actual
 echo "Buscando y copiando imagenes a directorio creado..." &&
-find $(pwd) -type f \( -name "*.png" -o -name "*.jpg" -o -name "*.jpeg" -o -name "*.svg" -o -name "*.raw" -o -name "*.gif" -o -name "*.heif" -o -name "*.mbp" -o -name "*.tiff" -o -name "*.psd" \) -exec cp {} ./mis_imagenes/ \; &&
+find $(pwd) -type f \( -name "*.png" -o -name "*.jpg" -o -name "*.jpeg" -o -name "*.svg" -o -name "*.raw" -o -name "*.gif" -o -name "*.heif" -o -name "*.mbp" -o -name "*.tiff" -o -name "*.psd" \) -exec cp {} ./my-images/ \; &&
 echo "Imagenes guardadas en directorio"
 
 
